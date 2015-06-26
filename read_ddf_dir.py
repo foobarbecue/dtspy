b0d6ab2b89abbc2b98e62e37525f2957907c1123
Aaron Curtis <foobarbecue@gmail.com>
2015-06-26 04:38:01 +0000

    Plots pretty nicely now and reader accepts a list of directories instead of a single directory.

--- read_ddf_dir.py
+++ read_ddf_dir.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
 import os, datetime
 import pandas
+import numpy
 from matplotlib import pyplot
 
 pyplot.ion() #set up interactive plotting
-datadir='/home/aaron/work/phd/3ddts/SN411049/data/full data set/MammothKnitting/channel 4/2013/jan/'
+datadirs=['/home/aaron/work/phd/3ddts/SN411049/data/full data set/MammothKnitting/channel 4/2013/jan/',
+          '/home/aaron/work/phd/3ddts/SN411049/data/full data set/MammothKnitting/channel 4/2012/dec/']
 
-def read_dts_dir(datadir, channel=4):
-    for filepath in os.listdir(datadir):
-        if filepath.endswith('.ddf'):
-            timestamp = datetime.datetime.strptime(filepath, 'channel ' + str(channel) + ' %Y%m%d %H%M%S 00001.ddf')
-            data_for_timestamp = pandas.read_csv(
-                datadir + filepath,
-                skiprows = 25,
-                delimiter ='\t')
-            data_for_timestamp.set_index('length (m)', inplace=True)
-            # would be better to do data_for_timestamp['temperature (°C)'] but the degree symbol is a problem
-            try:
-                data[timestamp] = data_for_timestamp.ix[:,0]
-            except NameError:
-                # first iteration
-                data = pandas.DataFrame(data_for_timestamp.ix[:,0],columns=[timestamp])
-            print("finished reading in " + filepath)
+def read_dts_dirs(datadirs=datadirs, ddf_column=0, channel=4):
+    for datadir in datadirs:
+        for filepath in os.listdir(datadir):
+            if filepath.endswith('.ddf') and 'channel ' + str(channel) in filepath:
+                timestamp = datetime.datetime.strptime(filepath, 'channel ' + str(channel) + ' %Y%m%d %H%M%S 00001.ddf')
+                data_for_timestamp = pandas.read_csv(
+                    datadir + filepath,
+                    skiprows = 25,
+                    delimiter ='\t')
+                data_for_timestamp.set_index('length (m)', inplace=True)
+                # would be better to do data_for_timestamp['temperature (°C)'] but the degree symbol is a problem
+                try:
+                    data[timestamp] = data_for_timestamp.ix[:,ddf_column]
+                except NameError:
+                    # first iteration
+                    data = pandas.DataFrame(data_for_timestamp.ix[:,ddf_column],columns=[timestamp])
+                #print("finished reading in " + filepath)
     data.sort(axis=1, inplace=True)
     return data
 
@@ -29,10 +32,14 @@ def plot_dts(dts_dataframe, min_dist=None, max_dist=None, min_time=None, max_tim
     plotax = pyplot.axes()
     myplot = plotax.pcolorfast(dts_dataframe.ix[min_dist:max_dist,min_time:max_time])
     pyplot.colorbar(myplot, ax=plotax)
-    pyplot.show()
+    locs, labels = pyplot.xticks()
+    #For some reason an extra tick is created beyond the end of the data. Remove it using [:-1].
+    locs, labels = locs[:-1], labels[:-1]
+    xdates = dts_dataframe.iloc[0,locs].index
+    pyplot.xticks(locs, xdates, rotation=45)
 
-plot_dts(dts_dataframe = read_dts_dir(datadir),
-         min_dist = 300,
-         max_dist = 600,
-         min_time = '2013-01-03 13:31:07',
-         max_time = '2013-01-03 13:31:07')
+plot_dts(dts_dataframe = read_dts_dirs(datadirs),
+        min_dist = 10,
+        max_dist = 690,
+        min_time = 0,
+        max_time = -60)
