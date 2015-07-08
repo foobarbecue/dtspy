6ac8bd8cabcfac7844691be67a6b45332b8b7dfd
Aaron Curtis <foobarbecue@gmail.com>
2015-07-08 23:59:20 +0000

    Remove cruft (may put back in as example file), fix ticks.

--- dts.py
+++ dts.py
@@ -5,13 +5,11 @@ import numpy
 from matplotlib import pyplot
 
 pyplot.ion() #set up interactive plotting
-datadirs=['/home/aaron/work/phd/3ddts/SN411049/data/full data set/MammothKnitting/channel 4/2013/jan/',
-          '/home/aaron/work/phd/3ddts/SN411049/data/full data set/MammothKnitting/channel 4/2012/dec/']
 
-def read_dts_dirs(datadirs=datadirs, ddf_column=0, channel=4):
+def read_dts_dirs(datadirs, ddf_column=0):
     for datadir in datadirs:
         for filepath in os.listdir(datadir):
-            if filepath.endswith('.ddf') and 'channel ' + str(channel) in filepath:
+            if filepath.endswith('.ddf'):
                 timestamp = datetime.datetime.strptime(filepath, 'channel ' + str(channel) + ' %Y%m%d %H%M%S 00001.ddf')
                 data_for_timestamp = pandas.read_csv(
                     datadir + filepath,
@@ -24,7 +22,6 @@ def read_dts_dirs(datadirs=datadirs, ddf_column=0, channel=4):
                     # first iteration
                     data = pandas.DataFrame(data_for_timestamp.ix[:,ddf_column],columns=[timestamp])
                     ref_temps = pandas.DataFrame
-                #print("finished reading in " + filepath)
     data.sort(axis=1, inplace=True)
     return data
 
@@ -37,14 +34,3 @@ def plot_dts(dts_dataframe, min_dist=None, max_dist=None, min_time=None, max_tim
     locs, labels = locs[:-1], labels[:-1]
     xdates = dts_dataframe.iloc[0,locs].index
     pyplot.xticks(locs, xdates, rotation=45)
-
-temperature = read_dts_dirs(ddf_column=0, channel=4)
-#stokes = read_dts_dirs(ddf_column='Stokes', channel=4)
-#anti_stokes = read_dts_dirs(ddf_column='anti-Stokes', channel=4)
-
-
-plot_dts(dts_dataframe = temperature,
-        min_dist = 10,
-        max_dist = 690,
-        min_time = 0,
-        max_time = -60)
