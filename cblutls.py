d3e6997ce8cf29b6b3f429839e3165dc72f8a390
Aaron Curtis <foobarbecue@gmail.com>
2015-07-09 17:48:57 +0000

    Getting close to being able to apply distance control points.

--- cblutls.py
+++ cblutls.py
@@ -17,7 +17,7 @@ def read_imsurvey_polyline(filepath):
     '''
     Read in the output of InnovMetric IMSurvey's "export polyline to text"
     '''
-    return pandas.read_csv(filepath, sep=" ", comment="#", names=['x','y','z'])
+    return pandas.read_csv(filepath, sep=" ", comment="#", names=['x','y','z']) #TODO import in segments
 
 def points_to_vectors(xyz):
     '''
@@ -28,14 +28,29 @@ def points_to_vectors(xyz):
     ijk.iloc[0] = xyz.iloc[0] #diff leaves NaNs in the first row, we want the position vector
     
     #Calculate euclidean distance from previous point
-    dists = numpy.sqrt(numpy.sum(numpy.square(ijk), axis=1))
+    dists = numpy.sqrt(numpy.sum(ijk**2, axis='columns'))
     cum_dists = numpy.cumsum(dists)
     
-    #Normalize the vectors
-    ijk_normd = ijk.divide(dists,axis='rows')
+    #Normalize the vectors and rename the columns
+    ijk_normd = ijk.divide(dists,axis='rows').columns(['i','j','k'])
     
     #Store distance and cumulative distance from first point along cable
     ijk['dist'] = dists
     ijk['cum_dists'] = numpy.cumsum(dists)
     
-    return ijk
+    #Return a rather redundant matrix containing:
+    ## xyz, ijk, distances, cumulative distances
+    return pandas.concat([xyz, ijk], axis='columns')
+
+def find_closest_point(xyz_array, xyz_point):
+    #Calculate distances between each point in xyz_array and xyz_point
+    dists_squared = numpy.sum((xyz_array-xyz_point)^2)
+    return numpy.argmin(dists_squared)
+
+def add_dist_control_points(xyz, control_file):
+    '''
+    control_file: a list of points that represent known cable distance / location pairs,
+    in the format x, y, z, dist
+    '''
+    closest_point_indices = xyz.apply(find_closest_point)
+
