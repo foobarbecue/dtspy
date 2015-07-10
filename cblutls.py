a4f72e4f834c8b2c50380fe287733b20b0bd3cab
Aaron Curtis <foobarbecue@gmail.com>
2015-07-10 03:19:21 +0000

    CableSection class initializes properly now.

--- cblutls.py
+++ cblutls.py
@@ -4,55 +4,35 @@ __author__ = "Aaron Curtis <aarongc@nmt.edu>"
 __date__ = "2015-07-09"
 __version__ = '0.1'
 
-import pandas, numpy
-
-class CableSection():
-    '''Represents a section of DTS cable in 3D space'''
-    def __init__(self, polyline_filepath, dist_ref_pts_filepath, dts_data=None):
-        #Read in the output of InnovMetric IMSurvey's "export polyline to text"
-        xyz = pandas.read_csv(polyline_filepath, sep=" ", comment="#", names=['x','y','z'])
-        
-        #Read in cable distance reference points
-        dist_ref_pts = pandas.read_csv(dist_ref_pts_filepath, sep=" ", names=['x', 'y', 'z', 'dist'])
-        
-        #Find indices of the closest polyline points to the reference points
-        closest_indices = dist_ref_pts.apply(find_closest_point)
-        
-        #Insert reference points into xyz
-        
-        
-        self.xyzijk = pts_to_vectors(self.xyz)
+import pandas, numpy, ipdb
 
 def pts_to_vectors(xyz):
     '''
     Convert a matrix of x, y, z points to a matrix of normalized vectors with
     rows of [xdiff, ydiff, yzdiff, distance_from_last_point, cumulative distance]
     '''
-    ijk = xyz.diff()
-    ijk.iloc[0] = xyz.iloc[0] #diff leaves NaNs in the first row, we want the position vector
+    ijk = xyz[['x','y','z']].diff()
+    #ijk.iloc[0] = xyz.iloc[0] #diff leaves NaNs in the first row, we want the position vector
     
     #Calculate euclidean distance from previous point
     dists = numpy.sqrt(numpy.sum(ijk**2, axis='columns'))
     cum_dists = numpy.cumsum(dists)
     
     #Normalize the vectors and rename the columns
-    ijk_normd = ijk.divide(dists,axis='rows').columns(['i','j','k'])
+    ijk = ijk.divide(dists,axis='rows')
+    ijk.columns = ['i','j','k']
     
     #Store distance and cumulative distance from first point along cable
-    ijk['dist'] = dists
-    ijk['cum_dists'] = numpy.cumsum(dists)
-    
+    ijk['euc_dist'] = dists
+
     #Return a rather redundant matrix containing:
     ## xyz, ijk, distances, cumulative distances
     return pandas.concat([xyz, ijk], axis='columns')
     
-def find_closest_point(xyz_array, xyz_point):
+def find_2closest_points(xyz_array, xyz_point):
     #Calculate distances between each point in xyz_array and xyz_point
     dists_squared = numpy.sum((xyz_array-xyz_point[:3])**2, axis='columns')
-    closest_pt_index = numpy.argmin(dists_squared)
-    print 'found a point ' + str(numpy.sqrt(dists_squared[closest_pt_index])) + ' away'    
-    #Return index of closest point
-    return xyz_array.ix[closest_pt_index]
+    return xyz_array.ix[numpy.argpartition(dists_squared,2)[:2]]
 
 def find_loc_btw_2pts(a, b, dist):
     a = numpy.array(a)
@@ -68,3 +48,21 @@ def add_dist_control_pts(xyz, control_file):
     '''
     closest_point_indices = xyz.apply(find_closest_point)
 
+class CableSection():
+    '''Represents a section of DTS cable in 3D space'''
+    def __init__(self, polyline_filepath, dist_ref_pts_filepath):
+        #Read in the output of InnovMetric IMSurvey's "export polyline to text"
+        xyz = pandas.read_csv(polyline_filepath, sep=" ", comment="#", names=['x','y','z'])
+        
+        #Read in cable distance reference points
+        dist_ref_pts = pandas.read_csv(dist_ref_pts_filepath, sep=" ", names=['x', 'y', 'z', 'ref_dist'], index_col=False)
+        
+        for pt in dist_ref_pts.values: #TODO rewrite using apply()
+            closest2 = find_2closest_points(xyz, pt)
+            #insert point between those two
+            upper = xyz.ix[closest2.index[0]:]
+            lower = xyz.ix[:closest2.index[1]]
+            pt = pandas.DataFrame([pt], columns=['x','y','z','dist']) #TODO this is bad
+            pt['is_distref']=True
+            xyz = pandas.concat([upper, pt, lower]).reset_index(drop=True)
+        self.xyzijk = pts_to_vectors(xyz)
